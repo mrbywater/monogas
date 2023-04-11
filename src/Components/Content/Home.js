705089a70c27baeb0404a0fb9fd35103caca1544
Николай Кабаченко <nick.kabachenko@gmail.com>
2023-04-11 14:15:59 +0300

    Xxx

---
+++ src/Components/Content/Home.js
@@ -0,0 +1,10 @@
+
+const Home = () =>{
+
+    return (
+        <div>hz</div>
+    )
+
+}
+
+export {Home}
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.scss"
+const Header = () =>{
+
+    return (
+        <div className="headerCont">privet</div>
+    )
+
+}
+
+export {Header}
