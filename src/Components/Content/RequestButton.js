9bf9d5b90753ccf72c8ea4c3ea92a01f94b9239c
Николай Кабаченко <nick.kabachenko@gmail.com>
2023-04-20 21:56:36 +0300

    Just info for some pages

--- src/Components/Content/RequestButton.js
+++ src/Components/Content/RequestButton.js
@@ -2,11 +2,11 @@ import "./RequestButton.scss"
 
 const RequestButton = () => {
     return (
-        <div className="buttonCont">
+        <a href="https://forms.gle/YQdJPVQ7meeYaxRC6" className="buttonCont">
             <div className="requestButton">
                 Оставить заявку
             </div>
-        </div>    
+        </a>
     )
 }
 
